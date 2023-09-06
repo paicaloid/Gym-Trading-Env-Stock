afbfea8f76ab92768e701af02bee65b06a1f01e5
praewptr <panthira.ruj@gmail.com>
2023-09-06 07:34:25 +0000

    add function

--- src/utils/enviroments.py
+++ src/utils/enviroments.py
@@ -1,19 +1,23 @@
 import gymnasium as gym
+import numpy as np
+import pandas as pd
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 
 # from src.stock_trading_env.single_stock_env import SingleStockTradingEnv
 
 
 def env_create(
-    env_name,
-    name,
-    df,
-    windows,
-    positions,
-    initial_position,
-    trading_fees,
-    portfolio_initial_value,
-    max_episode_duration,
-    verbose,
+    env_name: str,
+    name: str,
+    df: pd.DataFrame,
+    windows: int,
+    positions: list[int],
+    initial_position: int,
+    trading_fees: float,
+    portfolio_initial_value: float,
+    max_episode_duration: str,
+    verbose: int,
 ) -> gym.Env:
     env = gym.make(
         env_name,
@@ -27,10 +31,57 @@ def env_create(
         # reward_function=reward_function,
         portfolio_initial_value=portfolio_initial_value,  # in FIAT (here, USD)
         max_episode_duration="max",
-        verbose=1,
+        verbose=verbose,
     )
     return env
 
 
-def add_metric():
-    pass
+def add_metric(env: gym.Env):
+    env.add_metric("Reward", lambda history: history["reward"][-1])
+    env.add_metric(
+        "Portfolio Valuation", lambda history: history["portfolio_valuation"][-1]
+    )
+    return env
+
+
+def model_create(
+    env: gym.Env,
+    n_steps: int,
+    batch_size: int,
+    gae_lambda: float,
+    gamma: float,
+    n_epochs: int,
+    ent_coef: float,
+    verbose: int,
+    policy_kwargs: dict,
+    tensorboard_log: str,
+):
+    env = Monitor(env)
+    model = PPO(
+        "MlpPolicy",
+        env,
+        n_steps=1024,
+        batch_size=64,
+        gae_lambda=0.98,
+        gamma=0.999,
+        n_epochs=4,
+        ent_coef=0.01,
+        verbose=1,
+        policy_kwargs=policy_kwargs,
+        tensorboard_log="./PPO_tensorboard/",
+    )
+    return model
+
+
+def test_process(env, model):
+    reward_tot = 0
+    obs, info = env.reset()
+    done = False
+    truncated = False
+    while not done and not truncated:
+        obs = obs[np.newaxis, ...]
+        action, _ = model.predict(obs)
+        obs, reward, done, truncated, info = env.step(action[0])
+        reward_tot += reward
+
+    return env, reward_tot
